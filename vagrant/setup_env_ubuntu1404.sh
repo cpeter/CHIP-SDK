548724ed3c89229537761758d9fb33254994510f
Alex Kaplan <kaplan2539@gmail.com>
2015-08-18 12:20:42 -0700

    added 33 bit compatibility libraries and u-boot-tools

--- vagrant/setup_env_ubuntu1404.sh
+++ vagrant/setup_env_ubuntu1404.sh
@@ -10,7 +10,14 @@ sudo apt-get -y install \
  device-tree-compiler \
  libncurses-dev \
  cu \
- linux-image-extra-virtual
+ linux-image-extra-virtual \
+ u-boot-tools
 
-echo -e "\n Adding current user to dialout group"
-sudo add ${USER} dialout
+if uname -a |grep -q 64;
+then
+  echo -e "\n Installing 32bit compatibility libraries"
+  sudo apt-get install libc6-i386 lib32stdc++6 lib32z1
+fi
+
+#echo -e "\n Adding current user to dialout group"
+#sudo add ${USER} dialout
