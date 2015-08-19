8c55d265ef6e4e1008a731f49ab4554c4ad62568
Alex Kaplan <kaplan2539@gmail.com>
2015-08-19 16:07:12 -0700

    added a -y

--- vagrant/setup_env_ubuntu1404.sh
+++ vagrant/setup_env_ubuntu1404.sh
@@ -18,7 +18,7 @@ sudo apt-get -y install \
 if uname -a |grep -q 64;
 then
   echo -e "\n Installing 32bit compatibility libraries"
-  sudo apt-get install libc6-i386 lib32stdc++6 lib32z1
+  sudo apt-get -y install libc6-i386 lib32stdc++6 lib32z1
 fi
 
 #echo -e "\n Adding current user to dialout group"
